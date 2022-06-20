9a381a8ae7f43dff3ba026472a1b5a0039c29018
DmitriyGovosyants <Demiangvs@gmail.com>
2022-06-20 18:38:59 +0300

    fix: upload btn fix

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './js/mobile-menu';
 import './js/lang-menu';
 import './js/color-theme';
 import './js/back-to-top-btn';
-import './js/form-careers-handler';
+// import './js/form-careers-handler';
+import './js/form-careers-handler-2';
