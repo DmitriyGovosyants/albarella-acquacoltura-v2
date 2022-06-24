939ae7af907cf4fdc4e5f02332540793caf0f51f
DmitriyGovosyants <Demiangvs@gmail.com>
2022-06-24 09:07:55 +0300

    fix: file upload button and add close modal when click outside

--- src/js/modal-careers.js
+++ src/js/modal-careers.js
@@ -7,16 +7,24 @@ function handleModalOpen() {
   document.body.classList.add('modal-open');
   refs.backdrop.classList.remove('backdrop--hidden');
   document.addEventListener('keydown', handleModalCloseByEsc);
+  window.addEventListener('click', handleFormOutsideClick);
 }
 
 function handleModalClose() {
   document.body.classList.remove('modal-open');
   refs.backdrop.classList.add('backdrop--hidden');
   document.removeEventListener('keydown', handleModalCloseByEsc);
+  window.removeEventListener('click', handleFormOutsideClick);
 }
 
 function handleModalCloseByEsc({code}) {
   if (code === "Escape") {
+    handleModalClose();
+  }
+}
+
+function handleFormOutsideClick(e) {
+  if (e.target === refs.backdrop) {
     handleModalClose();
   }
 }
