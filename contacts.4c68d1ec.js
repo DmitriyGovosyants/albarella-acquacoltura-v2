eb95278154a9d5ccfb1ac38267b1fc4901b46617
DmitriyGovosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-07-07 14:26:58 +0000

    Deploying to gh-pages from @ DmitriyGovosyants/albarella-acquacoltura-v2@53561ab135fd9fb8d4b1ceee8f669e6c4d751e5b 🚀

---
+++ contacts.4c68d1ec.js
@@ -0,0 +1,2 @@
+function initMap(){var o={lat:45.0576389,lng:12.3537222},n=new google.maps.Map(document.querySelector(".google-map__init"),{zoom:12,center:o});new google.maps.Marker({position:o,map:n})}window.initMap=initMap;
+//# sourceMappingURL=contacts.4c68d1ec.js.map
--- contacts.62b5b3bb.js
+++
@@ -1,2 +0,0 @@
-function initMap(){var o={lat:45.0576389,lng:12.3537222},e=new google.maps.Map(document.querySelector(".google-map"),{zoom:10,center:o});new google.maps.Marker({position:o,map:e})}window.initMap=initMap;
-//# sourceMappingURL=contacts.62b5b3bb.js.map
