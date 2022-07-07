37172b6d42ef4de44cf511cfed27f56f2a51f16a
DmitriyGovosyants <Demiangvs@gmail.com>
2022-07-07 17:58:33 +0300

    feat: hidden arrow in map

--- src/js/google-map-toggle.js
+++ src/js/google-map-toggle.js
@@ -2,6 +2,12 @@ import { refs } from "./refs";
 
 refs.mapBtn.addEventListener('click', handleMapOpen);
 
-function handleMapOpen(e) {
+function handleMapOpen() {
   refs.map.classList.toggle('google-map__init--is-visible');
+
+  if (refs.map.classList.contains('google-map__init--is-visible')) {
+    return document.documentElement.style.setProperty("--display", "none");
+  }
+  
+  document.documentElement.style.setProperty("--display", "block");
 }
